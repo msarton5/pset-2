b9cb58fe450689037522004700ab0008f1835fd7
Molly Sarton <MSarton@ucvts.tec.nj.us>
2019-10-21 13:27:20 -0400

    moved files, added area and hello-user

---
+++ src/area.js
@@ -0,0 +1,5 @@
+const readlineSync = require("readline-sync");
+let Length = readlineSync.question("What is the length? ");
+const Length_mm = Length*25.4
+let Width = readlineSync.question("What is the width? ");
+const Width_mm = Width*25.4
