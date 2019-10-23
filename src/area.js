690c808efcd99151c3f70cca2c24bc89954d1f7f
Molly Sarton <MSarton@ucvts.tec.nj.us>
2019-10-23 13:23:14 -0400

    modified area.js

--- src/area.js
+++ src/area.js
@@ -1,5 +1,6 @@
 const readlineSync = require("readline-sync");
-let Length = readlineSync.question("What is the length? ");
-const Length_mm = Length*25.4
 let Width = readlineSync.question("What is the width? ");
 const Width_mm = Width*25.4
+let Length = readlineSync.question("What is the length? ");
+const Length_mm = Length*25.4
+console.log("A(n) " + Width + " by" + Length + " inch sheet of paper has an area of " + Width_mm*Length_mm + " millimeters.");
